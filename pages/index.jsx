78ddda9875e1723ccf7100d64cea670166ecceb5
MarkCodering <mark.chen.sstm@gmail.com>
2024-06-23 15:53:05 +0800

    first commit

---
+++ pages/index.jsx
@@ -0,0 +1,98 @@
+import { useEffect } from "react";
+import { useTheme } from "next-themes";
+import Head from "next/head";
+import Image from "next/image";
+
+import Contact from "@/components/Contact";
+import Experience from "@/components/Experience";
+
+import { BsArrowUpRight } from "react-icons/bs";
+
+import Avatar_Black from "@/public/Avatar-black.svg";
+import MarkImage from "@/public/mark.jpg";
+import Avatar_White from "@/public/Avatar-white.svg";
+import BooBlack from "@/public/boo-black.svg";
+import BooWhite from "@/public/boo-white.svg";
+import Link from "next/link";
+
+export default function Home() {
+  const { systemTheme, theme, setTheme } = useTheme();
+  const currentTheme = theme === "system" ? systemTheme : theme;
+
+  useEffect(() => {
+    console.clear();
+    console.log.apply(console, [
+      "%c Designed and Developed by Mark Chen %c %c🚀 %c\n",
+      "color: #fff; background: #8000ff; padding:5px 0;",
+      "color: #fff; background: #242424; padding:5px 0 5px 5px;",
+      "background: #242424; padding:5px 0",
+      "background: #242424; padding:5px 5px 5px 0",
+    ]);
+    console.log.apply(console, [
+      "%c Build, Learn, and Iterate %c\n",
+      "color: #fff; background: #8000ff; padding:5px 0;",
+    ]);
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>Mark Chen</title>
+        <link rel="icon" href="/Avatar-white.svg" />
+      </Head>
+      <div className="lg:min-h-screen px-10 sm:px-20 md:px-32 lg:mb-12 lg:px-60 mx-auto max-w-[75rem]">
+        <div className="lg:h-screen 2xl:h-max max-w-[90rem] mx-auto">
+          <div className="flex flex-row justify-start items-center mt-20">
+            <Image
+              src={currentTheme === "dark" ? MarkImage : MarkImage}
+              alt="Mark Chen"
+              width="80"
+              height="80"
+              style={{ borderRadius: "50%" }}
+            />
+            <div className="flex flex-col ml-4">
+              <h2 className="flex sm:text-2xl md:text-2xl lg:text-2xl">
+                <span className="font-semibold">Mark Chen</span>
+              </h2>
+              <h3>
+                <Link
+                  href={"https://www.linkedin.com/in/mark-chen-next"}
+                  target="_blank"
+                  className="text-[#717171bb] flex items-center"
+                >
+                  <p>@Mark Chen</p>
+                  <BsArrowUpRight className="stroke-1 h-3" />
+                </Link>
+              </h3>
+            </div>
+          </div>
+          <div className="mt-12 text-base md:text-xl lg:text-xl xl:text-xl 2xl:text-xl">
+            <p className="sm:leading-6 md:leading-6 lg:leading-8">
+              Mark is a computer science student at the University of London's
+              international program. In addition to his undergraduate studies,
+              Mark is a tech entrepreneur building an AI solution company to
+              help more software engineers adopt more generative AI innovations
+              on time. Moreover, he is a research scientist working with
+              professors, students, and other top institutions in Taiwan to
+              advance machine intelligence with quantum computing and innovative
+              algorithm design. He has published two papers with an m-index of 2
+              and more than 1600 article accesses in less than 6 months. Mark
+              primarily focuses on alignment research for LLM, engineering
+              practical quantum algorithms, and delivering on-demand courses for
+              engineers to learn more about generative AI. Mark's working and
+              engineering principle facilitates iterative and continuous growth
+              to build, learn, and iterate his products, solutions, and
+              scientific works.
+            </p>
+          </div>
+        </div>
+        <div className="mt-28 mx-auto">
+          <Experience />
+        </div>
+        <div className="mt-28 mx-auto ">
+          <Contact />
+        </div>
+      </div>
+    </>
+  );
+}
